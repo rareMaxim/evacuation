037a3a155c7563cd129dc333f7125dfe0b387a97
rareMax <maks4a@gmail.com>
2023-06-03 12:21:41 +0000

    Init

---
+++ evacuation/evacuation/doctype/evacuation_driver/evacuation_driver.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2023, Maxim Sysoev and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EvacuationDriver(Document):
+	# this method will run every time a document is saved
+    # def before_save(self):
+    #     self.full_name = f'{self.last_name} {self.first_name} {self.middle_name} '
+	pass
