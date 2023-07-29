659ca609bae73b00f52b4b31ef660806219aa8cb
Max S <maks4a@gmail.com>
2023-07-29 22:55:48 +0000

    wip

--- evacuation/evacuation/doctype/evacuation_car_request/evacuation_car_request.py
+++ evacuation/evacuation/doctype/evacuation_car_request/evacuation_car_request.py
@@ -2,8 +2,23 @@
 # For license information, please see license.txt
 
 # import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EvacuationCarRequest(Document):
-	pass
+    def add_car(self, car):
+        new_car = frappe.new_doc('Evacuation Car Response')
+        new_car.date = car.date
+        new_car.car = car.car
+        new_car.insert()
+        
+    def on_submit(self):
+        print('66666666666666666666666')
+        cars = frappe.get_all('Evacuation Car Request Table',
+                       filters = dict(parent=self),
+                       fields = ['date', 'car'])
+        for car in cars:
+            print(car)
+            self.add_car(car)
+        
--- evacuation/evacuation/doctype/evacuation_car_response/evacuation_car_response.js
+++ evacuation/evacuation/doctype/evacuation_car_response/evacuation_car_response.js
@@ -1,9 +1,8 @@
 // Copyright (c) 2023, Maxim Sysoev and contributors
 // For license information, please see license.txt
 
+// frappe.ui.form.on("Evacuation Car Response", {
+// 	refresh(frm) {
 
-frappe.ui.form.on("Evacuation Car Request", {
-	refresh(frm) {
-		console.log(frappe);
-	},
-});
+// 	},
+// });
